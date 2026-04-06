9702197c727c1a1e769693316137cdf9b6f375ab
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-04-07 00:10:10 +0600

    explored useNavigation in routing

--- src/assets/components/root/Root.jsx
+++ src/assets/components/root/Root.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Header from "../header/Header";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "../footer/Footer";
 import Sidebar from "../sidebar/Sidebar";
 import "./Root.css";
 
 const Root = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <div>
       <Header></Header>
       <div className="mainArea">
         <Sidebar></Sidebar>
+        {isNavigating && <span>Loading....</span>}
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
